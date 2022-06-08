f901b6ca73bcb4cc8b88eaa1a9166aebd8968be6
Ray Maestas <rjmaestas1991@gmail.com>
2022-06-08 13:41:30 -0600

    Add vacations to friends

--- friends.rb
+++ friends.rb
@@ -3,3 +3,9 @@ arrays = ["mike", "tom", "matt", "kevin", "ed"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+vacations = ["yosemite", "denver", "seattle", "yellowstone", "alaska"]
+
+vacations.push("france", "japan")
+
+puts vacations[5]
